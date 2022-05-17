36baeef43b9eae43ca714ee01d17943b37675fdd
anurag kevit <anurag.pindoriya@kevit.io>
2022-05-17 13:05:15 +0530

    Finally increment and dcrement using useReducer

--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import React, { useState, useReducer } from "react";
+// =============================================
 function Test() {
-  return <h1>Hello world</h1>;
+  
+  // =============================================
+  function reducer(state, action) {
+    console.log(state);
+    if(action.type == "inc")
+    {
+        
+        console.log("INR trigerd");
+        return state + 1;
+    }
+    else{
+       
+        console.log("dcr trigerd");
+        return state - 1;
+    }
+
+    return state + 1;
+  }
+
+  //   const [state, dispatch] = useReducer(reducer, 0);
+  const [state, dispatch] = useReducer(reducer, 0);
+  // =============================================
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h1>
+        <p>{state}</p>
+      </h1>
+      <button onClick={()=>dispatch({type : "inc"})}>+</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={()=>dispatch({type : "dcr"})}>-</button>
+    </>
+  );
 }
 
 export default Test;
